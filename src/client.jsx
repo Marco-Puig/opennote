d81e33d997abb44577a992c47ab7b2118e5f94a8
CarlosVillatoro <a7xcharlie18@gmail.com>
2024-03-07 10:51:02 -0500

    Initial commit for my new React Native Expo project

--- src/client.jsx
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const URL = 'https://dsmzsdwcqosymcyvemmn.supabase.co'; 
-
-const API_KEY = process.env.REACT_APP_API_KEY;
-
-export const supabase = createClient(URL, API_KEY);
