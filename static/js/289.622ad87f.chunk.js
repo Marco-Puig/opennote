f010472fcc3a17f996ccee1f18aebdabbcef5342
Marco Puig <90495366+Marco-Puig@users.noreply.github.com>
2024-03-15 19:47:35 -0400

    Updates

---
+++ static/js/289.622ad87f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkopennote=self.webpackChunkopennote||[]).push([[289],{3289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.622ad87f.chunk.js.map
